cced3e1fb90d201cc409e7981b56d7bb2bb56651
Thiago Loiola <bens.quin@gmail.com>
2026-09-22 12:20:18 -0300

    22/09/2026 primeiro periodo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { PlayCircleIcon } from "lucide-react";
+import { DefaulFooter } from "./components/Footer/Index";
 
 export function App() {
   console.log("Oi");
@@ -19,6 +24,35 @@ export function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <DefaultInput
+              placeholder="Digite algo"
+              labelText="Task"
+              id="meuInput"
+              type="string"
+              defaultValue="Valor preenchido"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles />
+          </div>
+
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
+          </div>
+        </form>
+      </Container>
+
+      <Container>
+        <DefaulFooter />
       </Container>
     </>
   );
